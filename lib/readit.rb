e555e2044166890ca3230150694d230ee753a4cf
dongbin.li <mike.d.1984@gmail.com>
2012-01-28 17:24:06 +0800

    update api docs

--- lib/readit.rb
+++ lib/readit.rb
@@ -29,7 +29,9 @@ module Readit
 	end
 
 	class API
-		# Create a new Readit API client
+		# initializer if creating a new Readit API client
+		# @param access_token access_token of user
+		# @param access_token_secret access_token_secret of user
 		def initialize(access_token='',access_token_secret='')
 			if access_token == '' or access_token_secret == ''
 				raise ReaditError.new('have to provide access_token and access_token_secret')
@@ -46,41 +48,39 @@ module Readit
 		SITE_URL = 'https://www.readability.com/'
 
 		# Retrieve the base API URI - information about subresources.
-		# /
 		def resource_info
 			request(:get,'/')
 		end
 
 		# Retrieve a single Article, including its content. 
-		# /articles/{article_id}
+		# api rest address: /articles/{article_id}
+		# @param article_id the article_id 
 		def article(article_id)
 			request(:get,"/articles/#{article_id}")
 		end
 
 		# Retrieve the bookmarks collection. Automatically filtered to the current user.
-		# bookmark_id support
-		#
-		# /bookmarks?
+		# api rest address : /bookmarks? or /bookmarks/{bookmark_id}
+		# @args support
+		# bookmark_id (at most return one record)
 		# archive
 		# favorite
 		# domain
 		# added_since
-		# added_until&
+		# added_until
 		# opened_since
-		# opened_until&
-		# archived_since&
-		# archived_until&
-		# favorited_since&
-		# favorited_until&
-		# updated_since&
-		# updated_until&
-		# order&
-		# page&
-		# per_page&
-		# exclude_accessibility&
+		# opened_until
+		# archived_since
+		# archived_until
+		# favorited_since
+		# favorited_until
+		# updated_since
+		# updated_until
+		# order
+		# page
+		# per_page
+		# exclude_accessibility
 		# only_deleted
-		#
-		# /bookmarks/{bookmark_id}
 		def bookmarks(args={})
 			if args[:bookmark_id] and args[:bookmark_id]!=''
 				request(:get,"/bookmarks/#{args[:bookmark_id]}")
@@ -92,7 +92,8 @@ module Readit
 
 		# Add a bookmark. Returns 202 Accepted, meaning that the bookmark has been added but no guarantees are made as 
 		# to whether the article proper has yet been parsed.
-		#
+		# @param args args to bookmark a url
+		# url the address to bookmark
 		# favorite 0 or 1
 		# archive 0 or 1
 		def bookmark(args={})
@@ -100,7 +101,9 @@ module Readit
 		end
 
 		# Update a bookmark. Returns 200 on successful update.
-		# /bookmarks/{bookmark_id}
+		# api rest address : /bookmarks/{bookmark_id}
+		# @param bookmark_id bookmark to update
+		# @param args args to update the bookmark
 		# favorite 0 or 1
 		# archive 0 or 1
 		def update_bookmark(bookmark_id,args={})
@@ -108,11 +111,13 @@ module Readit
 		end
 
 		# archive a bookmark by id
+		# @param bookmark_id bookmark to archive
 		def archive(bookmark_id)
 			update_bookmark(bookmark_id,:archive=>1)
 		end
 
 		# favorite a bookmark by id
+		# @param bookmark_id bookmark_id to favorite
 		def favorite(bookmark_id)
 			update_bookmark(bookmark_id,:favorite=>1)
 		end
@@ -125,19 +130,19 @@ module Readit
 		# the site associated with this bookmark will not receive any contributions for this bookmark.
 		# Use archive! It's better.
 		# Returns a 204 on successful remove.
+		# @param bookmark_id bookmark to delete
 		def delete_bookmark(bookmark_id)
 			request(:delete,"/bookmarks/#{bookmark_id}")
 		end
 
 		# Retrieve the contributions collection, which is a set of payments by a user to a specific domain. Automatically filtered to the current user.
-		#
-		# /contributions?since&until&domain&page&per_page
+		# api rest address : /contributions?since&until&domain&page&per_page
 		def contributions(args={})
 			request(:get,"/contributions",args)
 		end
 
 		# Retrieve the current user's information.
-		# /users/_current
+		# api rest address :/users/_current
 		def me
 			request(:get,"/users/_current")
 		end
