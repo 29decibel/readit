d93275ef23d237aa9abc0322bd988542cf4a5483
29decibel <mikeli@fastmail.fm>
2016-04-02 13:24:09 -0700

    fix rspec version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,7 @@ end
 
 RSpec.configure do |c|
   c.mock_with :rspec
-  c.color_enabled = true
+  c.color = true
   # so we can use `:vcr` rather than `:vcr => true`;
   # in RSpec 3 this will no longer be necessary.
   c.treat_symbols_as_metadata_keys_with_true_values = true
